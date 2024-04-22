e768d15d410752cfd8cb2afacd7b7fd5fb886ca6
David C. O'Gara <davidogara@hotmail.co.uk>
2024-04-22 14:53:56 +0100

    Add edits to video model

--- videos/models.py
+++ videos/models.py
@@ -28,6 +28,7 @@ class Region(models.Model):
     def __str__(self):
         return self.region
 
+    
 class Video(models.Model):
     """
     """
@@ -42,23 +43,25 @@ class Video(models.Model):
     ]
 
     FORMAT_CHOICES=[
-        ("1", "DVD"),
-        ("2", "Blu-Ray"),
-        ("3", "4K Ultra HD"),
-        ("4", "Dual Format: Blu-Ray and DVD"),
-        ("5", "Dual Format: UHD and Blu-Ray"),
+        ("DVD", "DVD"),
+        ("Blu-Ray", "Blu-Ray"),
+        ("4K Ultra HD", "4K Ultra HD"),
+        ("Dual Format: Blu-Ray and DVD", "Dual Format: Blu-Ray and DVD"),
+        ("Dual Format: UHD and Blu-Ray", "Dual Format: UHD and Blu-Ray"),
     ]
     
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, editable=True)
     certificate = models.CharField(choices=CERTIFICATE_CHOICES, max_length=250, default="1", null=False)
-    format = models.CharField(choices=FORMAT_CHOICES, max_length=250, default="1", null=False)
+    format = models.CharField(choices=FORMAT_CHOICES, max_length=250, default="DVD", null=False)
     discs = models.IntegerField(default=1, null=False)
     price = models.DecimalField(default=0.0, null=False, max_digits=4, decimal_places=2)
     cover = models.ImageField(null=True, blank=True)
     cover_url = models.URLField(max_length=1024, null=True, blank=True)
     overview = models.TextField(max_length=500, blank=True)
-    release_year = models.DecimalField(max_digits=4, decimal_places=0)
+    release_year = models.DecimalField(max_digits=4, decimal_places=0, blank=True)
+    aspect_ratio = models.CharField(max_length=250, blank=True)
+    feature_length = models.CharField(max_length=250, blank=True)
     added = models.DateTimeField(auto_now_add=True)
     on_sale = models.BooleanField(default=False)
     sku = models.CharField(max_length=254, null=True, blank=True)
@@ -70,5 +73,9 @@ class Video(models.Model):
         related_name='wishlist',
         blank=True
     )
+
+    def __str__(self):
+        return f'{self.title} {self.release_year} {self.format}'
+
 
     
