e254a68f09e56c044948131b09210574bd1f51ba
David C. O'Gara <davidogara@hotmail.co.uk>
2024-05-13 00:08:13 +0100

    Fix: change order number field made the uuid a seperate method as more reliable

--- checkout/models.py
+++ checkout/models.py
@@ -9,7 +9,7 @@ from videos.models import Video
 
 class CustomerOrder(models.Model):
 
-    order_number = models.UUIDField(default=uuid.uuid4(), null=False, editable=False)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     order_date = models.DateTimeField(auto_now_add=True)
 
     name = models.CharField(max_length=20, null=False, blank=False)
@@ -26,6 +26,13 @@ class CustomerOrder(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_basket = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
     
     def update_total(self):
         """
@@ -39,6 +46,15 @@ class CustomerOrder(models.Model):
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
 
 
     def __str__(self):
