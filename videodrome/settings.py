217fb49aa694fb40d2d5d9b70ae9a6ba998aa244
David C. O'Gara <davidogara@hotmail.co.uk>
2024-05-12 23:48:28 +0100

    Reinstall crispy-bootstrap5

--- videodrome/settings.py
+++ videodrome/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.stripe',
     'crispy_forms',
+    "crispy_bootstrap5",
     'django_countries',
     'main',
     'videos',
