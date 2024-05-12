8978d2940b356e8608d7f3943bf81fc98aa981a1
David C. O'Gara <davidogara@hotmail.co.uk>
2024-05-12 15:50:37 +0100

    Prep for heroku deployment

--- videodrome/settings.py
+++ videodrome/settings.py
@@ -100,6 +100,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware', # Enables django session use
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
