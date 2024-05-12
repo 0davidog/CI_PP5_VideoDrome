96a51140e4f915eaf597a56b7e537080a2abd8b7
David C. O'Gara <davidogara@hotmail.co.uk>
2024-05-12 22:25:06 +0100

    Add heroku url to allowed hosts

--- videodrome/settings.py
+++ videodrome/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://videodrome-ff881eaa1b51.herokuapp.com/']
 
 X_FRAME_OPTIONS = 'ALLOW-FROM https://ui.dev/amiresponsive'
 
