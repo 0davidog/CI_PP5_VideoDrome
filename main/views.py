7d486b2f1c2c45496c774af5e99d966462234cab
David C. O'Gara <davidogara@hotmail.co.uk>
2024-06-02 21:33:00 +0100

    Add simple email form for anon users

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.conf import settings
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -18,3 +22,34 @@ def index(request):
 
     # Render the index page with the provided context
     return render(request, 'main/index.html', context)
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            
+            send_mail(
+                subject,
+                message,
+                email,
+                [settings.EMAIL_HOST_USER],
+                fail_silently=False,
+            )
+            messages.success(request, 'Your message has been sent.')
+            return redirect('home')
+        
+        else:
+            # If the form is not valid, add an error message
+            messages.error(
+                request,
+                'Error submitting form. Please check your information.'
+                )
+
+    else:
+        form = ContactForm()
+    return render(request, 'main/contact_form.html', {'form': form})
