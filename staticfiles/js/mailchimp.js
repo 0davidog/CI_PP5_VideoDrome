ddc5b66ea302d5d152b64011258f7899b5559987
David C. O'Gara <davidogara@hotmail.co.uk>
2024-06-02 11:17:59 +0100

    Change heading type on footer

--- staticfiles/js/mailchimp.js
+++ staticfiles/js/mailchimp.js
@@ -1,3 +1,5 @@
 /*jshint esversion: 6 */
 
-(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);
+window.addEventListener('DOMContentLoaded', () => {
+    (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);
+});
--- staticfiles/js/toast.js
+++ staticfiles/js/toast.js
@@ -4,6 +4,11 @@
 /* Source: https://getbootstrap.com/docs/5.3/components/toasts/
 Reveals Bootstrap toast message */
 
-const messageToast = document.getElementById('message-toast');
-const toastBootstrap = bootstrap.Toast.getOrCreateInstance(messageToast);
-toastBootstrap.show();
+window.addEventListener('DOMContentLoaded', () => {
+
+    const messageToast = document.getElementById('message-toast');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(messageToast);
+
+    toastBootstrap.show();
+
+});
